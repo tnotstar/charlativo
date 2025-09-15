a659cf0beb528c0309c19500a429bada20ac80c5
Antonio Alvarado <tnotstar@gmail.com>
2025-09-15 14:06:05 +0000

    Initial Fiber setup

--- cmd/charla/main.go
+++ cmd/charla/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
-	fmt.Println("Hello, Charla!")
+	app := fiber.New(fiber.Config{
+		Views: html.New("./templates", ".html"),
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	app.Get("/chat", func(c *fiber.Ctx) error {
+		return c.Render("chat", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
